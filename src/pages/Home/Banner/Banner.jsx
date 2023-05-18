9a11dc8c7f49f6cba2e82cd28cd388c0ffdd18be
email <allentusar909@gmail.com>
2023-05-18 10:01:13 +0600

    add banner section

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div className=" py-8 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl mx-auto">
+        <div className="md:flex md:items-center md:justify-between">
+          {/* Text side */}
+          <div className="md:w-1/2 md:ml-16">
+            <h2 className="text-3xl font-extrabold text-gray-800">
+              CUTE TOYS FOR <br /> <span className="text-[red]">
+              CUTE KIDS
+              </span>
+            </h2>
+            <p className="mt-4 text-gray-600">
+              Discover a world of fun and adventure with our wide selection of toys.
+              Find the perfect superhero toy for your little one and make their day
+              memorable.
+            </p>
+            <button className="mt-6 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+              Shop Now
+            </button>
+          </div>
+
+          {/* Superhero toy side */}
+          <div className="mt-8 md:mt-0 md:ml-16 md:w-1/2">
+            <img
+              src="https://i.ibb.co/K7fQSMp/image.png"
+              alt="Superhero Toy"
+              className="h-auto w-full md:max-w-md mx-auto rounded-lg"
+            />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
-    return (
-        <div>
-            <h2>This is Home</h2>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
